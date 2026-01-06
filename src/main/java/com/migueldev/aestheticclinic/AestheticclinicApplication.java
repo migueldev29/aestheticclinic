eccecca11992dc11c9ebe4a7874a8417bc97a743
Miguel Francischini <migueldev29@gmail.com>
2026-01-06 20:35:06 -0300

    Spring Boot project

---
+++ src/main/java/com/migueldev/aestheticclinic/AestheticclinicApplication.java
@@ -0,0 +1,13 @@
+package com.migueldev.aestheticclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AestheticclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AestheticclinicApplication.class, args);
+	}
+
+}
