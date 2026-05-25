57e8f86b7ca2a3038f5e19bf6f98ede37fc3b6a6
Miguel Francischini <migueldev29@gmail.com>
2026-05-25 20:09:26 -0300

    Ajuste de biblioteca Pageable para UsuarioRepository

--- src/main/java/com/migueldev/aestheticclinic/repositories/UsuarioRepository.java
+++ src/main/java/com/migueldev/aestheticclinic/repositories/UsuarioRepository.java
@@ -3,8 +3,8 @@ package com.migueldev.aestheticclinic.repositories;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.migueldev.aestheticclinic.models.Usuario;
